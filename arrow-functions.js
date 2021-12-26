9685e804d4dde526c099d8d891657fb218501179
Sarah Jameson <meowells@gmail.com>
2021-12-26 14:35:13 +0000

    Exercise done

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,30 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(x, y) {
+    // code block
+    return x + y;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbers = (x, y) => {
+    return x + y;
+}
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers = (x, y) => x + y;
 
 // Implicit Returns
+const sayHello = () => console.log("Hello")
+sayHello()
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>
+    `
+)
+console.log(returnMultipleLines());
